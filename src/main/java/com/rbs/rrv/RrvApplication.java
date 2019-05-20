f4a12f2997a9d90ce4d9b45dc02941542fb38def
hibernatess <1959227206@qq.com>
2019-05-20 14:16:01 +0800

    初始化项目

---
+++ src/main/java/com/rbs/rrv/RrvApplication.java
@@ -0,0 +1,13 @@
+package com.rbs.rrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RrvApplication.class, args);
+    }
+
+}
